82ac3f41039e3f6a824cecb3da4ef0b00963ea0a
Vimal <92043391+vimalcodes@users.noreply.github.com>
2022-12-30 16:23:54 +0800

    Add files via upload

---
+++ includes/connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$con=mysqli_connect('localhost','root','','mystore');
+
+    if(!$con){
+        echo "connection successful";
+    } 
+
+?>
---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<div class="bg-primary p-3 text-center text-warning">
+    <p><strong>SEC Bootcamp 2022 - Vimal Ramanathan</strong></p>
+</div>
